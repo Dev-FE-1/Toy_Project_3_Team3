9de256d7ed0e69a20605d30eb1a17fd82b2851ba
Sonseongoh <nobleser@naver.com>
2024-08-21 13:14:58 +0900

    허스키 테스트

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,17 @@
 const Test = () => {
-	return <></>
+	return (
+		<>
+			<div>
+				<div>
+					<p>여긴 10칸</p>
+				</div>
+			</div>
+			<p>여긴 6칸</p>
+			<div>
+				<p>여긴 8칸</p>
+			</div>
+		</>
+	)
 }
 
 export default Test
